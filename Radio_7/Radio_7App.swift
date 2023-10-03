548c5b137dd767cedc0e16b9a2d00975b937a11e
@Annabel_ <hydrant94@gmail.com>
2023-10-03 23:10:23 +0200

    Initial Commit

---
+++ Radio_7/Radio_7App.swift
@@ -0,0 +1,17 @@
+//
+//  Radio_7App.swift
+//  Radio_7
+//
+//  Created by Аня Беликова on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Radio_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
